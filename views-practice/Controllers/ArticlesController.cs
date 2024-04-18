18d16fdbdd9abb9f48c24b0ee817ccf80d1dd662
hexatox <malki.adnane@univ-ouargla.dz>
2024-04-18 11:49:02 +0100

    Change picture in Contact us page

---
+++ views-practice/Controllers/ArticlesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace views_practice.Controllers
+{
+	public class ArticlesController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
