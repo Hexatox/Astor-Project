5985fddad894d395f9b406c278a04018bae42cf6
hexatox <malki.adnane@univ-ouargla.dz>
2024-04-18 10:35:18 +0100

    Add project files.

---
+++ views-practice/Controllers/CategoriesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace views_practice.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ views-practice/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using views_practice.Models;
+
+namespace views_practice.Controllers;
+
+public class HomeController : Controller
+{
+
+    
+
+
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+       
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public IActionResult Pricing()
+    {
+        return View();
+    }
+
+
+    public IActionResult Show()
+    {
+        
+
+        return View();
+    }
+
+
+
+}
