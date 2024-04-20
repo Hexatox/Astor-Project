fea7e24327c9771af6a940c09eba3ae316b8ce8f
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-04-20 11:55:01 -0300

    changes : 1-\ add new Class Library project for `Utilitties` 2-\ add Enum to descripe all (Sociel Media Linkes in the Project) 3-\ Imlement the `ApplicationUser` Entity this is main Enitity Reprisent the User 3-\ imlement and add the relation between user and his linkes

---
+++ Models/Entities/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Bio {  get; set; }
+        public string ProfilePhoto { get; set; }
+
+        //navigation Propriry for his Sociel Media Links
+        public IQueryable <SocielMediaUser> Links { get; set; }
+        
+
+    }
+    
+
+}
---
+++ Models/Entities/SocielMediaUser.cs
@@ -0,0 +1,22 @@
+﻿using Utility;
+using Utility.Enum;
+
+namespace Models.Entities
+{
+    
+    public class SocielMediaUser
+    {
+        public string Link { get; set; }
+
+        // relations / navigations
+
+
+        // those both <`ApplicationUserId`> and <`SocielMediaTypeId`> are Primary Key of this Entity
+        public int ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+                                                            
+        public SocialMediaType SocielMediaType { get; set; }
+        
+    }
+    
+}
