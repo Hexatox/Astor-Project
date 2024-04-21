27328543340dbee2bad40e23d307291a125fed39
hexatox <malki.adnane@univ-ouargla.dz>
2024-04-21 12:10:59 +0100

    Added Sign in page

---
+++ views-practice/Controllers/SignIn.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace views_practice.Controllers
+{
+    public class SignIn : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
