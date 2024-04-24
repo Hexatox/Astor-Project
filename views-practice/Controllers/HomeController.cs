85e3ae2b8f03410d14b77aac4129900dbcf71e7a
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-04-24 14:46:12 +0100

    remove my Test

--- views-practice/Controllers/HomeController.cs
+++ views-practice/Controllers/HomeController.cs
@@ -13,11 +13,9 @@ public class HomeController : Controller
 
 
     private readonly ILogger<HomeController> _logger;
-    private readonly UploadFileBlob uploadFileBlob;
-    public HomeController(ILogger<HomeController> logger , UploadFileBlob uploadFileBlob)
+    public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
-       this.uploadFileBlob = uploadFileBlob;
     }
 
     public IActionResult Index()
@@ -41,12 +39,6 @@ public class HomeController : Controller
         
 
         return View();
-    }
-
-    [HttpPost]
-    public string? Upload(IFormFile file)
-    {
-        return uploadFileBlob.Upload (file).GetAwaiter().GetResult();
     }
 
 }
