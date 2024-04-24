8e903f5471014f9a2fc9d38cc12e4a385b29322b
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-04-24 14:44:50 +0100

    fix the Upload File Class

--- views-practice/Controllers/HomeController.cs
+++ views-practice/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using views_practice.Models;
+using views_practice.Utility;
 
 namespace views_practice.Controllers;
 
@@ -12,11 +13,11 @@ public class HomeController : Controller
 
 
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly UploadFileBlob uploadFileBlob;
+    public HomeController(ILogger<HomeController> logger , UploadFileBlob uploadFileBlob)
     {
         _logger = logger;
-       
+       this.uploadFileBlob = uploadFileBlob;
     }
 
     public IActionResult Index()
@@ -42,6 +43,10 @@ public class HomeController : Controller
         return View();
     }
 
-
+    [HttpPost]
+    public string? Upload(IFormFile file)
+    {
+        return uploadFileBlob.Upload (file).GetAwaiter().GetResult();
+    }
 
 }
