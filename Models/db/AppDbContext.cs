fba43a92567ea448073eaac80e0b6f5ec6d28cb9
Houssam-re <rekibi.houssam@univ-ouargla.dz>
2024-05-01 12:25:02 +0100

    test

--- Models/db/AppDbContext.cs
+++ Models/db/AppDbContext.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Utility.Enum;
-
+//test
 namespace DAL.db
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser>
