2a35381a210d8cf40b2cd7b798c036326a1a352d
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-05-03 18:48:50 +0100

    User explicit Envirenment to test hosted Application

--- views-practice/Program.cs
+++ views-practice/Program.cs
@@ -17,8 +17,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Load configuration based on environment
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
-
+builder.Configuration.AddJsonFile($"appsettings.Production.json", optional: true, reloadOnChange: true);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConntection"))
