23e600cf55eda6ba8ff962c328cc9148768f997b
MOHAMMED AMIR BOUNGAB <boungab.mohammedamir@univ-ouargla.dz>
2024-05-09 21:20:47 +0100

    fix the White View in all Areas

--- views-practice/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ views-practice/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -28,16 +28,18 @@ namespace views_practice.Areas.Identity.Pages.Account
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
+            //if (returnUrl != null)
+            //{
+            //    return LocalRedirect(returnUrl);
+            //}
+            //else
+            //{
+            //    // This needs to be a redirect so that the browser performs a new
+            //    // request and the identity for the user gets updated.
+            //    //return RedirectToPage();
+            //    return RedirectToAction("Index" , "Home" , new {Area = "User"});
+            //}
+            return RedirectToAction("Index", "Home", new { Area = "User" });
         }
     }
 }
