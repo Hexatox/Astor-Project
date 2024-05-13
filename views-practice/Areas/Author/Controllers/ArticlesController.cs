1552466c92603df2a211ee8501efcda008b193db
hexatox <malki.adnane@univ-ouargla.dz>
2024-05-13 03:04:22 +0100

    Modification on add article page

--- views-practice/Areas/Author/Controllers/ArticlesController.cs
+++ views-practice/Areas/Author/Controllers/ArticlesController.cs
@@ -1,20 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BLL.Services.IServices;
+using DAL.Entities;
+using DAL.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.Protocol;
 
 namespace views_practice.Areas.Author.Controllers
 {
     [Area("Author")]
     public class ArticlesController : Controller
     {
+
+        private readonly ICatigoryService catigoryService; 
+
+        public ArticlesController(ICatigoryService catigoryService)
+        {
+            this.catigoryService = catigoryService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult NewArticle ()
+        public async Task<IActionResult>  NewArticle ()
         {
-            return View();
+            IEnumerable<Catigory> ctgrList = await catigoryService.GetAllAsync();
+
+
+
+
+            return View(ctgrList);
         }
-    }
+        [HttpPost]
+        public IActionResult AddPost(Post post )
+        {
+			return Ok(new { message = post.Title });
+
+
+
+		}
+
+
+
+	}
 
 
 
