46e50ec3436a98c1e0aa21078de6525a4cce937d
Adnane Malki <malki.adnane@univ-ouargla.dz>
2024-05-18 19:22:50 +0100

    Translate pages to Arabic

---
+++ views-practice/Areas/Author/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace views_practice.Areas.Author.Controllers
+{
+    [Area("Author")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
