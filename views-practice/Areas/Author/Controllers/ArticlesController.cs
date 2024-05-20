e788525065fda9f11cf95c1c48238f4fb4b37360
Adnane Malki <malki.adnane@univ-ouargla.dz>
2024-05-21 00:05:43 +0100

    Add post page

--- views-practice/Areas/Author/Controllers/ArticlesController.cs
+++ views-practice/Areas/Author/Controllers/ArticlesController.cs
@@ -2,7 +2,12 @@
 using DAL.Entities;
 using DAL.Repositories.IRepositories;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json.Linq;
 using NuGet.Protocol;
+using System.Collections.ObjectModel;
+using System.IdentityModel.Tokens.Jwt;
+using Utility.Enum;
 
 namespace views_practice.Areas.Author.Controllers
 {
@@ -10,18 +15,21 @@ namespace views_practice.Areas.Author.Controllers
     public class ArticlesController : Controller
     {
 
-        private readonly ICatigoryService catigoryService; 
+        private readonly ICatigoryService catigoryService;
+        private readonly IPostService postRepository; 
 
         public ArticlesController(ICatigoryService catigoryService)
         {
             this.catigoryService = catigoryService;
         }
 
+
+        [Area("User")]
         public IActionResult Index()
         {
             return View();
         }
-
+        [Area("Author")]
         public async Task<IActionResult> NewArticle()
         {
             IEnumerable<Catigory> ctgrList = await catigoryService.GetAllAsync();
@@ -40,11 +48,45 @@ namespace views_practice.Areas.Author.Controllers
 
         //}
         [HttpPost]
-        public IActionResult AddPost(Post post, List<int> categoryIds)
+        [Area("Author")]
+        public async Task<IActionResult> AddPost([FromBody] JObject jsonData)
         {
-            if (ModelState.IsValid)
+
+
+			Post post = new Post
+			{
+				Title = (string)jsonData["title"],
+				Content = (string)jsonData["content"],
+				Image = (string)jsonData["image"],
+				Likes = new Collection<Like>(),
+				PostDate = DateTime.Now,
+				Samary = (string)jsonData["samary"],
+				Catigories = new Collection<Catigory>(),
+				IsAviable = true,
+				PostType = PostType.Article,
+				TotalLikes = 0,
+			};
+
+			if (ModelState.IsValid)
+            {
+
+                
+                try
+                {
+                  await postRepository.CreatePostAsync(post);
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e);
+                }
+                
+                
+
+                return Ok(new { success = true });
+            }
             
-                return Ok(new { message = post.PostType });
+               
             return BadRequest();
            
            
--- views-practice/Areas/User/Controllers/CategoriesController.cs
+++ views-practice/Areas/User/Controllers/CategoriesController.cs
@@ -4,6 +4,8 @@ namespace views_practice.Areas.User.Controllers
 {
     public class CategoriesController : Controller
     {
+
+        [Area("User")]
         public IActionResult Index()
         {
             return View();
